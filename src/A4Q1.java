f7d20419c9fdf929b34b5f8987fb50f2f29e1bb8
micla1676 <micla1676@HRH-IC0027336.wrdsb.ca>
2016-10-20 09:19:00 -0400

    Completed A4Q1

---
+++ src/A4Q1.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+/**A4Q1
+ * program that asks for name
+ * @author micla1676
+ */
+public class A4Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //create the scanner
+        Scanner input = new Scanner(System.in);
+        
+        //ask user for their name
+        System.out.println("Please Enter your name:");
+        
+        //get the name from the user
+        String name = input.nextLine();
+        
+        System.out.println("Hello " + name + ". How are you today?");
+    }
+}
